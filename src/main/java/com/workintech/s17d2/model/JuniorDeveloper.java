648ebebedd63ea615d78fecac88a4bbce2718921
melisa-kabasakal <meliskabasakal33@gmail.com>
2025-02-05 19:11:47 +0300

    final project

---
+++ src/main/java/com/workintech/s17d2/model/JuniorDeveloper.java
@@ -0,0 +1,8 @@
+package com.workintech.s17d2.model;
+
+public class JuniorDeveloper extends Developer {
+
+    public JuniorDeveloper(int id, String name, Double salary) {
+        super(id, name, salary, Experience.JUNIOR);
+    }
+}
